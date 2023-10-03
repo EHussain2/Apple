5bb79148e7e66754b0a183fcffb38d387c0298fe
C3682952 <E.hussain5687@student.leedsbeckett.ac.uk>
2023-10-03 13:59:05 +0100

    Followed week 2 worksheet, added what is your name, and added the numbers +100 etc.

---
+++ practise 2.py
@@ -0,0 +1,20 @@
+if __name__== '__main__': print('hello world')
+total = 100
+
+print("The total is", total)
+
+total = total + 99
+
+print ("the total is now", total)
+total = total * 100
+print("the total is now", total)
+total = total - 1
+print("The total is", total)
+total = total * 4
+print("The total is", total)
+total = total / 2
+print("The total is", total)
+
+total = 98.2
+count = 5
+print("The average was", total/count)
--- practise.py
+++ practise.py
@@ -7,5 +7,10 @@ if __name__ == '__main__':
         year_born = int(input('And what year were you born? '))
 
         age = year_now - year_born
-
-        print(f'Hello, {name}. It is good to meet you. This year you will be {age} years old.')
+print(f'Hello, {name}. It is good to meet you. This year you will be {age} years old.')
+name_length = len("Eric Idle")
+print(print("The length of you name is", len(name)))
+age = input("Enter your age ")
+age = int(age)
+print("in one year your will be", age + 1)
+print("Hello there,\nWhat is your name?")
