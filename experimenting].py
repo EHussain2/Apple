a9bdb7fb018ccd1581044645882b2d16be702697
C3682952 <E.hussain5687@student.leedsbeckett.ac.uk>
2023-10-10 14:18:41 +0100

    added you are old things

---
+++ experimenting].py
@@ -0,0 +1,14 @@
+ageStr = input("what is your age?")
+age = int(ageStr)
+
+if age > 100
+    print("you are very old")
+    print("well done")
+elif age > 80:
+    print("you are fairly old")
+    print("pretty good")
+elif age >40:
+    print("you are middle aged")
+    print("never mind")
+else:
+    print("you are not very old -yet")
