c4bba879c55f3107dc59640d717550635fe38bd1
EHussain2 <E.hussain5687@student.leedsbeckett.ac.uk>
2023-11-06 23:30:38 +0000

    I managed to complete the part where if no response is recorded it shall move on.

---
+++ Test P1.py
@@ -0,0 +1,7 @@
+if __name__== '__main__':
+    name = input('Hello, What is your name?')
+
+    if len(name) > 0:
+        print(f'well met, {name} ! It"s great to meet you')
+    else:
+        print('well met kind stranger, How shall I learn your name?')
