c3d13910cc2be74a449d42ba6ea91121e6072682
EHussain2 <E.hussain5687@student.leedsbeckett.ac.uk>
2023-12-21 13:44:42 +0000

    part 1 of assessment

--- task1.py
+++ task1.py
@@ -1,22 +1,35 @@
-while True:
-    try:
+if __name__== '__main__':
+
         order_amount= int(input("How many pizzas would you like to order today?:"))
         if order_amount <= 1:
             print(" Please enter a valid amount")
-            continue
-            break
-    except ValueError:
-                print("please enter a valid number to continue")
-                amount = order_amount *12
-                print(f"Your total will be: £{amount}")
 
+elif order_amount >= 1:
+    print("please enter a valid number to continue")
+amount = order_amount *12
 print(f"Your current total is £{amount}, proceed to see total after discounts")
-while True:
+if True:
     tuesday = input("Is it Tuesday today? Yes or No? ")
     if tuesday == "yes":
-        amount = amount/2
-        print(f"Your new total with discount is £{amount}")
+        amount1 = amount/2
+        print(f"Your new total with discount is £{amount1}")
         if tuesday == "no":
             print("Unfortunately no discount can be applied this moment in time")
-            break
-        else:
+        elif tuesday == False:
+            print("invalid input, please enter yes or no")
+
+            delivery_cost = 2.50
+            amount1 = (amount + delivery_cost)
+            while True:
+                delivery = input("Do you need delivery? Yes or No?")
+
+                if delivery == "yes":
+                    if order_amount <= 5:
+                        print(f"The new total is £{delivery_cost}")
+                        print(f"As more than 5 pizzas have been ordered, you get free delivery.")
+                    break
+                if delivery == "no":
+                    print(" no delivery charge added")
+                    break
+                else:
+                    print("enter yes or no")
