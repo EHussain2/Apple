586fbd86038e54bcee94360d6e78040c7ef954d7
EHussain2 <E.hussain5687@student.leedsbeckett.ac.uk>
2023-12-24 09:49:23 +0000

    managed to get delivery charge added

--- task1.py
+++ task1.py
@@ -1,10 +1,10 @@
 if __name__== '__main__':
 
         order_amount= int(input("How many pizzas would you like to order today?:"))
-        if order_amount <= 1:
+        if order_amount <= 0:
             print(" Please enter a valid amount")
 
-elif order_amount >= 1:
+elif order_amount >= 0:
     print("please enter a valid number to continue")
 amount = order_amount *12
 print(f"Your current total is £{amount}, proceed to see total after discounts")
@@ -13,30 +13,21 @@ if True:
     if tuesday == "yes":
         amount1 = amount/2
         print(f"Your new total with discount is £{amount1}")
-        if tuesday == "no":
+        if tuesday == "no":\
             print("Unfortunately no discount can be applied this moment in time")
-        elif tuesday == False:
+        elif not tuesday:
             print("invalid input, please enter yes or no")
-
-            delivery_cost = 2.50
-            amount1 = (amount + delivery_cost)
-            while True:
+while True:
                 delivery = input("Do you need delivery? Yes or No?")
 
                 if delivery == "yes":
                     if order_amount <= 5:
                         print(f"The new total is £{delivery_cost}")
-                        print(f"As more than 5 pizzas have been ordered, you get free delivery.")
+                    else:
+                        print("As more than 5 pizzas have been ordered you've received free delivery")
                     break
-                if delivery == "no":
+                elif delivery == "no":
                     print(" no delivery charge added")
                     break
                 else:
                     print("enter yes or no")
-                App= delivery_cost/2
-            while True:
-                App = input("Are you using the app today? Yes or No?")
-
-                if delivery == "yes":
-                        print(f"The new total is £{delivery_cost}")
-                        print(f"As more than 5 pizzas have been ordered, you get free delivery.")
